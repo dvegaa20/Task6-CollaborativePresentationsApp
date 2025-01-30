66def54fa34927ee1aaff403c207086d1c37140f
dvegaa20 <diego.vega.camacho@gmail.com>
2025-01-30 14:13:11 -0600

    fix(TextEditor): revert socket connection to local URL for development

--- frontend/src/TextEditor.js
+++ frontend/src/TextEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState();
 
   useEffect(() => {
-    const s = io(process.env.MONGODB_URI);
+    const s = io("http://localhost:3001");
     setSocket(s);
 
     return () => {
